3314987a0d60dee489ed16309d89ff7c254482b2
alexm <alex-m@outlook.com>
2023-09-02 12:02:03 -0700

    Add logic to addVertex() and addVertices() methods

--- graph.js
+++ graph.js
@@ -15,11 +15,17 @@ class Graph {
     }
 
     // this function accepts a Node instance and adds it to the nodes property on the graph
-    addVertex(vertex) {}
+    addVertex(vertex) {
+        this.nodes.add(vertex);
+    }
 
     // this function accepts an array of Node instances and adds them to the nodes property on the
     // graph
-    addVertices(vertexArray) {}
+    addVertices(vertexArray) {
+        for (let vertex of vertexArray) {
+            this.addVertex(vertex);
+        }
+    }
 
     // this function accepts two vertices and updates their adjacent values to include the other
     // vertex

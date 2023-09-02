34250ce67415cc413258352cbc14393988daeb3e
alexm <alex-m@outlook.com>
2023-09-02 12:11:40 -0700

    Add logic to addEdge(), removeEdge(), removeVertex() methods

--- graph.js
+++ graph.js
@@ -29,21 +29,37 @@ class Graph {
 
     // this function accepts two vertices and updates their adjacent values to include the other
     // vertex
-    addEdge(v1, v2) {}
+    addEdge(v1, v2) {
+        v1.adjacent.add(v2);
+        v2.adjacent.add(v1);
+    }
 
     // this function accepts two vertices and updates their adjacent values to remove the other
     // vertex
-    removeEdge(v1, v2) {}
+    removeEdge(v1, v2) {
+        v1.adjacent.delete(v2);
+        v2.adjacent.delete(v1);
+    }
 
     // this function accepts a vertex and removes it from the nodes property, it also updates any
     // adjacency lists that include that vertex
-    removeVertex(vertex) {}
+    removeVertex(vertex) {
+        for (let adj of vertex.adjacent) {
+            this.removeEdge(vertex, adj);
+        }
+
+        this.nodes.delete(vertex);
+    }
 
     // this function returns an array of Node values using DFS
-    depthFirstSearch(start) {}
+    depthFirstSearch(start) {
+
+    }
 
     // this function returns an array of Node values using BFS
-    breadthFirstSearch(start) {}
+    breadthFirstSearch(start) {
+
+    }
 }
 
 
